5910c1404bf30dacf7f6a472c71ed8e08db404d0
dtaubaso <d.taubaso@gmail.com>
2024-10-24 10:21:57 -0300

    cambios en padding

--- app.py
+++ app.py
@@ -13,9 +13,7 @@ st.markdown("""
     <style>
             .block-container {
                     padding-top: 1rem;
-                    padding-bottom: 0rem;
-                    padding-left: 5rem;
-                    padding-right: 5rem;
+                   
                 }
     .input-box {
         font-size: 18px; /* Tamaño de texto en los input */
